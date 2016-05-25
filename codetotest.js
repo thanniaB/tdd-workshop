dbe6bff293bb6b1823816c3180581d2d2358083a
tblanchet <tblanchet@switchfly.com>
2016-05-25 01:15:00 +0000

    refactored code

--- codetotest.js
+++ codetotest.js
@@ -10,52 +10,55 @@
 //   return result;
 // }
 
-var rolls = [];
-var currentRoll = 0;
+var BowlingGame = (function () {
+  var rolls = [];
+  var currentRoll = 0;
 
-function isSpare(firstInFrame) {
-  return rolls[firstInFrame] + rolls[firstInFrame+1] == 10;
-}
-
-function isStrike(firstInFrame) {
-  return rolls[firstInFrame] == 10;
-}
-
-function nextBallForSpare(firstInFrame) {
-  return rolls[firstInFrame+2];
-}
-
-function nextTwoBallsForStrike(firstInFrame) {
-  return rolls[firstInFrame + 1] + rolls[firstInFrame + 2];
-}
-
-function twoBallsInFrame(firstInFrame) {
-  return rolls[firstInFrame] + rolls [firstInFrame + 1];
-}
-
-var BowlingGame = {
-  newGame: function() {
-    rolls = [];
-    currentRoll = 0;
-  },
-  roll: function(pins) {
-    rolls[currentRoll++] = pins;
-  },
-  score: function() {
-    var score = 0;
-    var firstInFrame = 0;
-    for(var frame = 0; frame < 10; frame++){
-        if(isStrike(firstInFrame)) {
-          score += 10 + nextTwoBallsForStrike(firstInFrame);
-          firstInFrame++;
-        } else if(isSpare(firstInFrame)) {
-          score += 10 + nextBallForSpare(firstInFrame);
-          firstInFrame+= 2;
-        } else {
-          score += twoBallsInFrame(firstInFrame);
-          firstInFrame += 2;
-        }
-    }
-    return score;
+  function isSpare(firstInFrame) {
+    return rolls[firstInFrame] + rolls[firstInFrame+1] == 10;
   }
-}
+
+  function isStrike(firstInFrame) {
+    return rolls[firstInFrame] == 10;
+  }
+
+  function nextBallForSpare(firstInFrame) {
+    return rolls[firstInFrame+2];
+  }
+
+  function nextTwoBallsForStrike(firstInFrame) {
+    return rolls[firstInFrame + 1] + rolls[firstInFrame + 2];
+  }
+
+  function twoBallsInFrame(firstInFrame) {
+    return rolls[firstInFrame] + rolls [firstInFrame + 1];
+  }
+
+  return {
+    newGame: function() {
+      rolls = [];
+      currentRoll = 0;
+    },
+    roll: function(pins) {
+      rolls[currentRoll++] = pins;
+    },
+    score: function() {
+      var score = 0;
+      var firstInFrame = 0;
+      for(var frame = 0; frame < 10; frame++){
+          if(isStrike(firstInFrame)) {
+            score += 10 + nextTwoBallsForStrike(firstInFrame);
+            firstInFrame++;
+          } else if(isSpare(firstInFrame)) {
+            score += 10 + nextBallForSpare(firstInFrame);
+            firstInFrame+= 2;
+          } else {
+            score += twoBallsInFrame(firstInFrame);
+            firstInFrame += 2;
+          }
+      }
+      return score;
+    }
+  }
+
+}());
